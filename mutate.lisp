7316bc42806b098cdf3b679e7b2757955ccbba1e
RuthM11 <ruth.madrid11@gmail.com>
2017-05-11 17:33:20 -0700

    mutate takes a list and replaces operator (#2)
    if the randomly generated number is 1, it will replace with a random operator based on another randomly generated number

---
+++ mutate.lisp
@@ -0,0 +1,37 @@
+(defun replace-nth (list n value)
+  "Recursively calls itself and replaces list's 'n'th element with 'value'"
+  (if (> n 0)
+      (cons (nth 0 list)
+	    ;; recursive call down the list
+            (replace-nth (cdr list) (1- n) value))
+      (cons value (cdr list))))
+
+(defun mutate(critter)
+    (write critter)
+   ;need generate random number from 1 to 100 for 1% chance of mutation
+   (setq randnum 1)
+   ;Then set it to num (1 for now)
+   (setq num randnum)
+    
+   ;need to generate random number from 1 to 100 for random operator
+   (setq randop 45)
+   ;then set it to op num
+   (setq opnum randop)
+   
+   ;then need to get random operator
+   (if (and (<= opnum 25) (> opnum) 0)
+      (setq oper '+))
+   (if (and (<= opnum 50) (> opnum 25))
+      (setq oper '-))
+   (if (and (<= opnum 75) (> opnum) 50)
+      (setq oper '*))
+   (if (and (<= opnum 100) (> opnum 75))
+      (setq oper '/))
+   
+   ;if the randomly generated number is 1, MUTATE!
+   (if (= num 1)
+     (setq critter (replace-nth critter 0 oper)))
+   (write critter)
+)
+;test
+;(mutate '(+ 2 3 4 5))
